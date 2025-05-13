0de3a892345bc681bf3b02ce7df660a979b950aa
Dave Tervoort <dave.tervoort@accenture.com>
2025-05-13 10:59:19 +0200

    Essent test master

---
+++ preview/Essent_E223/1080x1920/main.js
@@ -0,0 +1,56 @@
+window.onload = function () {
+    init();
+}
+
+function init() {
+   
+
+    document.querySelector("#mainExit").addEventListener("mouseover", function(){
+        if(masterTL.progress() == 1){
+            
+        }
+    })
+    
+    document.querySelector("#mainExit").addEventListener("mouseout", function(){
+        if(masterTL.progress() == 1){
+            
+        }
+    })
+
+    document.querySelector("#mainExit").addEventListener("click", function(){
+        window.open(clickTag, '_blank');
+        masterTL.progress(1).pause();
+    })
+
+    getAnimation();
+}
+
+function getAnimation(){
+    
+
+    masterTL = gsap.timeline({repeat:0});
+
+    masterTL.to("#loaderWrapper", 0.2, {opacity:0, ease:Sine.easeInOut, onComplete:function(){
+        gsap.set("#loaderWrapper", {display:"none"});
+    }}, 0)
+    masterTL.add("start");
+    masterTL.from("#bg", 2, {scale:1.2, ease:Sine.easeInOut}, "start");
+    masterTL.from("#pinkBorder", 2, {scale:3.75, ease:Sine.easeInOut}, "start");
+    masterTL.from("#quote", 0.75, {scale:0, x:-20, ease:Back.easeOut}, "start+=2.5");
+
+    masterTL.from("#text1", 0.5, {opacity:0, ease:Sine.easeInOut}, "start+=3");
+    masterTL.to("#price", 3, { innerText:150, snap: {innerText:1}, onUpdate:function(){
+        if(Number(this._targets[0].innerText) < 10){
+            gsap.set("#price", {width:50});
+        }
+        else if (Number(this._targets[0].innerText) < 100){
+            gsap.set("#price", {width:100});
+        }
+        else{
+            gsap.set("#price", {width:132});
+        }
+        
+    }, ease:Circ.easeInOut}, "start+=3.5");
+
+    masterTL.from("#text2", 0.5, {opacity:0, y:50, ease:Sine.easeInOut}, "start+=7");
+}
