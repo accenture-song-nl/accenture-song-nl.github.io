8ae620b4ad889fc15fb8a5c1c4f0602bd9da3a49
Dave Tervoort <dave.tervoort@accenture.com>
2025-06-10 11:25:20 +0200

    Amazon Romance master done

--- preview/Amazon_Book_2025/Romance/300x600/main.js
+++ preview/Amazon_Book_2025/Romance/300x600/main.js
@@ -27,11 +27,15 @@ function init() {
 
 function getAnimation(){
 
-    // var druidTL = animateDruid();
+    var birdTL = animateBird();
 
     rolloverTL = gsap.timeline({paused:true});
     rolloverTL.add("start");
     rolloverTL.to("#spaceWrapper", 0.5, {clipPath:"polygon(-93% -7%, 151% 0%, 31% 79%)", repeat:1, yoyo:true, ease:Sine.easeInOut}, "start");
+    rolloverTL.to("#birdWingR", 0.3, {rotationX:100, repeat:3, yoyo:true, ease:Power1.easeInOut}, "start");
+    rolloverTL.to("#birdWingL", 0.3, {rotationX:-100, repeat:3, yoyo:true, ease:Power1.easeInOut}, "start+=0.1");
+    rolloverTL.to("#couple", 0.5, {y:-10, repeat:1, yoyo:true, ease:Sine.easeInOut}, "start");
+    rolloverTL.to(["#scarfWrapper1", "#scarfWrapper2", "#scarfWrapper3", "#flame1", "#flame2"], 0.75, {y:-20, repeat:1, yoyo:true, ease:Sine.easeInOut}, "start");
 
     masterTL = gsap.timeline({repeat:2});
 
@@ -45,11 +49,15 @@ function getAnimation(){
     masterTL.from("#scarfWrapper2", 1, {width:0, ease:Sine.easeInOut}, "start+=2.8");
     masterTL.from("#scarfWrapper1", 1.2, {width:0, ease:Sine.easeInOut}, "start+=3.7");
     masterTL.from("#flame1", 0.5, {scale:0, ease:Sine.easeOut}, "start+=3.7");
+    masterTL.add(birdTL, "start+=4");
     masterTL.from("#flame1", 0.01, {rotationY:180, repeat:30, yoyo:true, repeatDelay:0.19, ease:Sine.easeOut}, "start+=4.3");
     masterTL.from("#flame2", 0.5, {scale:0, ease:Sine.easeOut}, "start+=4.5");
     masterTL.from("#flame2", 0.01, {rotationY:180, rotation:7, repeat:20, yoyo:true, repeatDelay:0.24, ease:Sine.easeOut}, "start+=5");
+    masterTL.from("#flower1", 0.5, {opacity:0, ease:Sine.easeOut}, "start+=6.5");
+    masterTL.from("#flower2", 0.5, {opacity:0, ease:Sine.easeOut}, "start+=6.8");
+    masterTL.from("#flower3", 0.5, {opacity:0, ease:Sine.easeOut}, "start+=7.1");
+    masterTL.from("#flower4", 0.5, {opacity:0, ease:Sine.easeOut}, "start+=7.4");
     
-    // masterTL.add(druidTL, "start+=2");
     masterTL.to("#banner", 0.5, {}, "start+=9.3");
 
     console.log(masterTL.duration());
@@ -57,13 +65,14 @@ function getAnimation(){
 
 }
 
-// function animateDruid() {
-//     var tl = gsap.timeline({});
+function animateBird() {
+    var tl = gsap.timeline({});
 
-//     tl.add('start');
-//     tl.to("#druid", 3.5, {x:-230, y:50, ease:Sine.easeIn}, "start");
-//     tl.to("#druid", .5, {marginTop:5, repeat:6, yoyo:true, ease:Sine.easeInOut}, "start");
+    tl.add('start');
+    tl.from("#birdWrapper", 4, {x:-150, y:340, ease: "back.out(1)"}, "start");
+    tl.to("#birdWingR", 0.3, {rotationX:100, repeat:13, yoyo:true, ease:Power1.easeInOut}, "start");
+    tl.to("#birdWingL", 0.3, {rotationX:-100, repeat:13, yoyo:true, ease:Power1.easeInOut}, "start+=0.1");
 
-//     return tl;
-// }
+    return tl;
+}
 
