40b8d537b868f5b9db1e0c2dc1fdf84672b48b23
Wessel <wessel.hooghiemstra@accenture.com>
2025-07-04 11:10:16 +0200

    added amazon fantasy 320x100 EN & DE

---
+++ preview/Amazon_Book_2025/History/320x100/main.js
@@ -0,0 +1,86 @@
+window.onload = function () {
+    init();
+}
+
+function init() {
+
+    document.querySelector("#mainExit").addEventListener("mouseover", function () {
+        if (masterTL.progress() == 1) {
+            rolloverTL.play(0);
+        }
+    })
+    
+    document.querySelector("#mainExit").addEventListener("mouseout", function(){
+        if(masterTL.progress() == 1){
+        }
+    })
+
+    document.querySelector("#mainExit").addEventListener("click", function(){
+        window.open(clickTag, '_blank');
+        masterTL.progress(1).pause();
+    })
+
+    getAnimation();
+}
+
+
+function getAnimation(){
+
+    var vikingTL = animateViking();
+
+    rolloverTL = gsap.timeline({paused:true});
+    rolloverTL.add("start");
+
+    masterTL = gsap.timeline({repeat:2});
+
+    masterTL.to("#loaderWrapper", 0.2, {opacity:0, ease:Sine.easeInOut, onComplete:function(){
+        gsap.set("#loaderWrapper", {display:"none"});
+    }}, 0)
+    masterTL.add("start");
+    masterTL.from("#copy", 1, {clipPath:"polygon(50% 0%, 50% 0%, 50% 100%, 50% 100%)", ease:Sine.easeInOut, onComplete:function(){
+        gsap.set("#copy", {opacity:0});
+        gsap.set("#copy2", {opacity:1});
+    }}, "start");
+
+    masterTL.to(vikingTL, 10,{progress:1, ease:Sine.easeOut}, "start");
+
+    masterTL.to("#fire1", 1.6,{scaleX:1.1, scaleY:1.3, repeat:4, yoyo:true, ease:Sine.easeInOut}, "start");
+    masterTL.to("#fire1", 2,{rotation:5, repeat:3, yoyo:true, ease:Sine.easeInOut}, "start");
+
+    masterTL.from("#castleFire2", 0.5,{scale:0, ease:Sine.easeOut}, "start+=1.4");
+    masterTL.to("#castleFire2", 1.6,{scaleX:1.1, scaleY:1.3, repeat:4, yoyo:true, ease:Sine.easeInOut}, "start+=1.9");
+    masterTL.to("#castleFire2", 2,{rotation:5, repeat:3, yoyo:true, ease:Sine.easeInOut}, "start+=1.9");
+    
+    masterTL.from("#castleFire4", 0.7, {scale:0, ease:Sine.easeOut}, "start+=1");
+    masterTL.to("#castleFire4", 2,{scaleX:1.2, scaleY:0.8, repeat:3, yoyo:true, ease:Sine.easeInOut}, "start+=1.7");
+    masterTL.to("#castleFire4", 1.6,{rotation:20, repeat:3, yoyo:true, ease:Sine.easeInOut}, "start+=1.7");
+
+    masterTL.from("#castleFire1", 1,{scale:0, ease:Sine.easeOut}, "start+=2");
+    masterTL.to("#castleFire1", 0.625,{scaleX:0.9, scaleY:1.2, repeat:7, yoyo:true, ease:Sine.easeInOut}, "start+=3");
+    masterTL.to("#castleFire1", 1.25,{rotation:-5, repeat:3, yoyo:true, ease:Sine.easeInOut}, "start+=3");
+    
+    masterTL.from("#castleFire5", 1,{scale:0, ease:Sine.easeOut}, "start+=2.5");
+    masterTL.to("#castleFire5", 0.64285714285,{scaleX:0.9, scaleY:1.1, repeat:6, yoyo:true, ease:Sine.easeInOut}, "start+=3.5");
+    masterTL.to("#castleFire5", 1.5,{rotation:-5, repeat:2, yoyo:true, ease:Sine.easeInOut}, "start+=3.5");
+    
+    masterTL.from("#castleFire3", 0.5,{scale:0, ease:Sine.easeOut}, "start+=3");
+    masterTL.to("#castleFire3", 0.64285714285,{scaleX:1.2, scaleY:1.1, repeat:6, yoyo:true, ease:Sine.easeInOut}, "start+=3.5");
+    masterTL.to("#castleFire3", 1.5,{rotation:5, repeat:2, yoyo:true, ease:Sine.easeInOut}, "start+=3.5");
+
+    masterTL.to("#banner", 0.5, {}, "start+=9.3");
+    
+    console.log(masterTL.duration());
+}
+
+function animateViking() {
+    var tl = gsap.timeline({repeat:0});
+
+    tl.add('start');
+    tl.from("#vikingWrapper", 9, {x:100, ease:Power0.easeNone}, "start");
+    tl.from("#shield", 1.8, {x:-25, y:-10, yoyo:true, repeat:4, ease:Sine.easeInOut}, "start");
+    tl.from("#vikingWrapper", 1.8, {y:10, yoyo:true, repeat:4, ease:Sine.easeInOut}, "start");
+    
+    tl.to("#vikingFire", 1.8, {rotation:12, scaleX:1.2, scaleY:0.9, yoyo:true, repeat:4, ease:Sine.easeInOut}, "start");
+
+    return tl;
+}
