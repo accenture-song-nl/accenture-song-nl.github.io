3b3130f4aa979d9a4eccf07b6c79cbe74962bd23
Dave Tervoort <dave.tervoort@accenture.com>
2025-08-28 22:01:52 +0200

    LVM feedback on master

--- preview/LVM/300x600/main.js
+++ preview/LVM/300x600/main.js
@@ -19,7 +19,8 @@ function init(clickTAGvalue) {
 
     document.querySelector("#mainExit").addEventListener("click", function(){
         clickTAGvalue = dhtml.getVar('clickTAG', 'http://www.example.com');
-        masterTL.progress(1).pause();
+        // masterTL.progress(1).pause();
+        masterTL.pause();
         
     })
 
@@ -38,8 +39,8 @@ function getAnimation(){
 
     masterTL.add("start");
     masterTL.from("#bg1", 1.5, {scale:1.2, ease:Sine.easeInOut}, "start")
-    masterTL.from(["#footer", "#logo", "#gradient"], 1, {y:200, ease:Sine.easeOut}, "start+=1")
-    masterTL.from("#text1", 0.5, {x:-170, ease:Sine.easeOut}, "start+=2")
+    // masterTL.from(["#footer", "#logo", "#gradient"], 1, {y:250, ease:Sine.easeOut}, "start+=1")
+    masterTL.from("#text1", 0.5, {x:-250, ease:Sine.easeOut}, "start+=2")
     masterTL.to(["#bg1", "#text1"], 0.5, {opacity:0, ease:Sine.easeOut}, "start+=4")
     
     masterTL.add("frame2", "-=0.5");
@@ -49,15 +50,16 @@ function getAnimation(){
 
     masterTL.add("frame3", "-=0.5");
     masterTL.from("#bg3", 1.5, {scale:1.2, ease:Sine.easeInOut}, "frame3")
-    masterTL.from("#text3A", 0.5, {x:-170, ease:Sine.easeOut}, "frame3+=1")
+    masterTL.from("#text3A", 0.5, {x:-250, ease:Sine.easeOut}, "frame3+=1")
     masterTL.from("#text3B", 0.5, {x:-250, ease:Sine.easeOut}, "frame3+=1.2")
-    masterTL.from("#text3C", 0.5, {x:-170, ease:Sine.easeOut}, "frame3+=1.4")
+    masterTL.from("#text3C", 0.5, {x:-250, ease:Sine.easeOut}, "frame3+=1.4")
+    masterTL.from("#text3D", 0.5, {x:-250, ease:Sine.easeOut}, "frame3+=1.6")
     masterTL.from("#cta", 0.5, {scale:0, ease:Back.easeOut}, "frame3+=4")
-    masterTL.to(["#bg3", "#text3A", "#text3B", "#text3C"], 0.5, {opacity:0, ease:Sine.easeOut}, "frame3+=6")
+    masterTL.to(["#bg3", "#text3A", "#text3B", "#text3C", "#text3D"], 0.5, {opacity:0, ease:Sine.easeOut}, "frame3+=6")
     
     masterTL.add("frame4", "-=0.5");
     masterTL.from("#bg4", 1.5, {scale:1.2, ease:Sine.easeInOut}, "frame4")
-    masterTL.from("#text4A", 0.5, {x:-170, ease:Sine.easeOut}, "frame4+=1")
+    masterTL.from("#text4A", 0.5, {x:-250, ease:Sine.easeOut}, "frame4+=1")
     masterTL.from("#text4B", 0.5, {x:-250, ease:Sine.easeOut}, "frame4+=1.2")
     
     masterTL.add("end", "+=1");
