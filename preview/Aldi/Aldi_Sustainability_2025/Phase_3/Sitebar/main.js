7e224d8e85750673b12700fb3e87df2e67be6b33
Dave Tervoort <dave.tervoort@accenture.com>
2025-09-01 15:47:55 +0200

    Sust phase 3 SB done

--- preview/Aldi/Aldi_Sustainability_2025/Phase_3/Sitebar/main.js
+++ preview/Aldi/Aldi_Sustainability_2025/Phase_3/Sitebar/main.js
@@ -22,11 +22,13 @@ function init() {
     })
 
     document.querySelector("#mainExit").addEventListener("click", function(){
-        masterTL.progress(1);
+        // masterTL.progress(1);
+        masterTL.pause();
     })
 }
 
 function onResize(){
+    var screenW = document.body.offsetWidth;
     var bannerH = document.querySelector("#banner").offsetHeight;
     var bannerW = document.querySelector("#banner").offsetWidth;
     var bannerS = bannerH/1200;
@@ -37,9 +39,11 @@ function onResize(){
         var newScale = bannerW/(scalerW/2);
         gsap.set("#scaler", {left:"50%", xPercent:-50, transformOrigin:"50% 0%", scale:newScale*bannerS});
         gsap.set("#extraScaler", {scale:newScale*bannerS});
+        gsap.set("#extraInfo", {x:-300});
     }
     else if(bannerW < scalerW){
         gsap.set("#scaler", {left:"50%", xPercent:-50, transformOrigin:"50% 0%"});
+        gsap.set("#extraInfo", {x:-((scalerW - screenW)/2)/bannerS});
     }
     else{
         gsap.set("#scaler", {left:"0%", xPercent:0, transformOrigin:"0% 0%"});
@@ -48,39 +52,34 @@ function onResize(){
 
 function getAnimation(){
 
-    masterTL = gsap.timeline({paused:true});
+    masterTL = gsap.timeline({paused:true, repeat:1});
     masterTL.add("start")
     masterTL.to("#loaderWrapper", 0.2, {opacity:0, ease:Sine.easeInOut}, 0)
     masterTL.add("start");
-    masterTL.from("#bgBorder", 0.5, {scale:3, y:200, ease:Sine.easeOut}, "start")
-    masterTL.from("#text1", 0.5, {y:500, ease:Sine.easeOut}, "start+=0.5")
-    masterTL.from("#text1B", 0.5, {y:500, ease:Sine.easeOut}, "start+=1.2")
+    masterTL.from("#bgBorder", 0.5, {scale:4, y:100, ease:Sine.easeOut}, "start")
+    masterTL.from("#text1", 0.5, {y:1000, ease:Sine.easeOut}, "start+=0.5")
+    masterTL.from("#text1B", 0.5, {y:1000, ease:Sine.easeOut}, "start+=1.2")
     masterTL.from("#text1HighlightWrapper", 0.7, {width:0, ease:Sine.easeInOut}, "start+=1.9")
 
     masterTL.add("showFrame2", 3)
-    masterTL.from("#text2", 0.5, {y:1000, ease:Sine.easeOut}, "showFrame2+=1.5")
-    masterTL.to(["#text2"], 0.5, {opacity:0, ease:Sine.easeInOut}, "showFrame2+=4.5")
+    // masterTL.to(["#bgBorder", "#bg"], 0.5, {y:-250, ease:Sine.easeIn}, "showFrame2+=1")
+    masterTL.to(["#text1", "#text1B", "#text1HighlightWrapper"], 0.5, {y:-1200, ease:Sine.easeInOut}, "showFrame2+=1")
+    masterTL.from("#text2", 0.5, {y:1200, ease:Sine.easeOut}, "showFrame2+=1.5")
+    masterTL.from("#extraInfo", 0.5, {opacity:0, ease:Sine.easeOut}, "showFrame2+=1.75")
+    masterTL.from("#carrot", 1, {y:1200, ease: "back.out(0.5)"}, "showFrame2+=2")
+    masterTL.to(["#text2", "#carrot", "#extraInfo", "#bg", "#logo"], 0.5, {opacity:0, ease:Sine.easeInOut}, "showFrame2+=7")
 
-    masterTL.add("showStickerFrame")
-    masterTL.from("#sticker", 0.5, {y:1000, ease:Sine.easeOut}, "showStickerFrame")
-    // masterTL.from("#sticker2", 0.5, {y:1000, ease:Sine.easeOut}, "showStickerFrame+=0.2")
-    // masterTL.to(["#text1", "#text1HighlightWrapper", "#sticker", "#sticker2"], 0.5, {opacity:0, ease:Sine.easeInOut}, "showStickerFrame+=3.5")
-    masterTL.to(["#text1", "#text1B", "#text1HighlightWrapper", "#sticker"], 0.5, {opacity:0, ease:Sine.easeInOut}, "showStickerFrame+=3.5")
-
-    masterTL.add("showFrame3")
-
-    masterTL.to(["#bgBorder", "#bg"], 0.5, {y:-1000, ease:Sine.easeIn}, "showFrame3")
-    masterTL.to("#logo", 0.5, {opacity:0, ease:Sine.easeInOut}, "showFrame3+=1")
-    masterTL.from("#logo2", 0.3, {opacity:0, ease:Sine.easeOut}, "showFrame3+=1.2")
-    masterTL.from("#text4HighlightWrapper", 0.5, {y:1000, ease:Sine.easeOut}, "showFrame3+=2")
-    masterTL.from(["#text3HighlightWrapper"], 0.5, {width:0, ease:Sine.easeOut}, "showFrame3+=2.7")
+    masterTL.add("showFrame3", "-=0.3")
+    masterTL.from("#logo2", 0.3, {opacity:0, ease:Sine.easeOut}, "showFrame3")
 
     masterTL.add("endScreen")
     
     masterTL.from("#ctaLeft", 0.5, {scale:0, transformOrigin:"50% 95%", ease:Back.easeOut}, "endscreen");
     masterTL.from("#ctaRight", 0.01, {opacity:0, ease:Sine.easeOut}, "endscreen+=0.7");
     masterTL.from("#ctaRight", 0.3, {x:-50, ease:Sine.easeOut}, "endscreen+=0.7");
- 
+    
+    masterTL.to("#banner", 0.5, {}, "14.5");
+
     masterTL.play();
 
     console.log(masterTL.duration());
