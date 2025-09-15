4d11044723a3386720eee0c22555d6368a94f140
Dave Tervoort <dave.tervoort@accenture.com>
2025-09-15 16:28:40 +0200

    Owned brands feedback

--- preview/Aldi/Aldi_Owned_Brands/Sitebar/main.js
+++ preview/Aldi/Aldi_Owned_Brands/Sitebar/main.js
@@ -52,33 +52,33 @@ function getAnimation(){
     masterTL.to("#loaderWrapper", 0.2, {opacity:0, ease:Sine.easeInOut, onComplete:function(){
         gsap.set("#loaderWrapper", {display:"none"});
     }}, 0)
-    masterTL.from("#product", 17, {x:150, ease:Sine.easeOut}, "intro")
-    masterTL.from("#productLogoWrapper", 1, {width:0, ease:Sine.easeInOut}, "intro+=0.5")
+    // masterTL.from("#product", 17, {x:150, ease:Sine.easeOut}, "intro")
+    // masterTL.from("#productLogoWrapper", 1, {width:0, ease:Sine.easeInOut}, "intro+=0.5")
     
-    masterTL.from("#bgBorder", 0.5, {scale:2.2, ease:Sine.easeOut}, "intro+=1.5")
-    masterTL.from("#text1", 0.5, {opacity:0, x:-100, ease:Sine.easeOut}, "intro+=2")
-    masterTL.from("#highlight1Wrapper", 0.5, {width:0, ease:Sine.easeOut}, "intro+=2.5")
+    masterTL.from("#bgBorder", 0.5, {scale:2.2, ease:Sine.easeOut}, "intro+=0.5")
+    masterTL.from("#text1", 0.5, {opacity:0, x:-100, ease:Sine.easeOut}, "intro+=1")
+    masterTL.from("#highlight1Wrapper", 0.5, {width:0, ease:Sine.easeOut}, "intro+=1.5")
 
-    masterTL.to(["#text1", "#highlight1Wrapper"], 0.5, {opacity:0, x:100, ease:Sine.easeIn}, "intro+=5")
+    masterTL.to(["#text1", "#highlight1Wrapper"], 0.5, {opacity:0, x:100, ease:Sine.easeIn}, "intro+=4")
     
-    masterTL.to("#bgBorder", 0.5, {height:902, ease:Sine.easeOut}, "intro+=5.5")
+    masterTL.to("#bgBorder", 0.5, {height:902, ease:Sine.easeOut}, "intro+=4.5")
     
-    masterTL.from("#text2", 0.5, {opacity:0, x:-100, ease:Sine.easeOut}, "intro+=6")
-    masterTL.to("#text2", 0.5, {opacity:0, x:100, ease:Sine.easeIn}, "intro+=8")
+    masterTL.from("#text2", 0.5, {opacity:0, x:-100, ease:Sine.easeOut}, "intro+=5")
+    masterTL.to("#text2", 0.5, {opacity:0, x:100, ease:Sine.easeIn}, "intro+=7")
 
-    masterTL.from("#text3", 0.5, {opacity:0, x:-100, ease:Sine.easeOut}, "intro+=8.5")
-    masterTL.to("#text3", 0.5, {opacity:0, x:100, ease:Sine.easeIn}, "intro+=10.5")
+    masterTL.from("#text3", 0.5, {opacity:0, x:-100, ease:Sine.easeOut}, "intro+=7.5")
+    masterTL.to("#text3", 0.5, {opacity:0, x:100, ease:Sine.easeIn}, "intro+=9.5")
 
-    masterTL.from("#text4", 0.5, {opacity:0, x:-100, ease:Sine.easeOut}, "intro+=11")
-    masterTL.to("#text4", 0.5, {opacity:0, x:100, ease:Sine.easeIn}, "intro+=13")
+    masterTL.from("#text4", 0.5, {opacity:0, x:-100, ease:Sine.easeOut}, "intro+=10")
+    masterTL.to("#text4", 0.5, {opacity:0, x:100, ease:Sine.easeIn}, "intro+=12")
 
-    masterTL.from("#sticker", 0.5, {opacity:0, x:-100, ease:Sine.easeOut}, "intro+=13.5")
+    masterTL.from("#sticker", 0.5, {opacity:0, x:-100, ease:Sine.easeOut}, "intro+=12.5")
 
-    masterTL.from("#endBg", 0.5, {opacity:0, ease:Sine.easeIn}, "intro+=15.5")
-    masterTL.from("#endLogo", 0.5, {opacity:0, y:20, ease:Sine.easeIn}, "intro+=16")
-    masterTL.from("#ctaLeft", 0.5, {scale:0, transformOrigin:"50% 70%", ease:Back.easeOut}, "intro+=16.5");
-    masterTL.from("#ctaRight", 0.01, {opacity:0, ease:Sine.easeOut}, "intro+=17.2");
-    masterTL.from("#ctaRight", 0.3, {x:-50, ease:Sine.easeOut}, "intro+=17.2");
+    masterTL.from("#endBg", 0.5, {opacity:0, ease:Sine.easeIn}, "intro+=14.5")
+    masterTL.from("#endLogo", 0.5, {opacity:0, y:20, ease:Sine.easeIn}, "intro+=15")
+    masterTL.from("#ctaLeft", 0.5, {scale:0, transformOrigin:"50% 70%", ease:Back.easeOut}, "intro+=15.5");
+    masterTL.from("#ctaRight", 0.01, {opacity:0, ease:Sine.easeOut}, "intro+=16.2");
+    masterTL.from("#ctaRight", 0.3, {x:-50, ease:Sine.easeOut}, "intro+=16.2");
     
     masterTL.play();   
 }
