81f9f18d81ae966702d011430b657f37af47ad6e
Dave Tervoort <dave.tervoort@accenture.com>
2025-09-23 16:15:43 +0200

    UKG all sizes UK

---
+++ preview/UKG/UKG_728x90_Schedule/localDynamic.js
@@ -0,0 +1,36 @@
+localDynamicData = {
+	"variationName": "UK_Schedule",
+	"campaignName": "UK",
+	"dimensionName": {
+		"160x600": "UKG_160x600_Schedule",
+		"300x250": "UKG_300x250_Schedule",
+		"300x600": "UKG_300x600_Schedule",
+		"728x90": "UKG_728x90_Schedule",
+		"970x250": "UKG_970x250_Schedule"
+	},
+	"bg": "./bg.jpg",
+	"logo": "./logo.png",
+	"text1": {
+		"160x600": "Work works<BR>when the right<BR>skills are in the<BR>right places",
+		"300x250": "Work works<BR>when the right<BR>skills are in the<BR>right places",
+		"300x600": "Work works when<BR>the right skills are<BR>in the right places",
+		"728x90": "Work works when the right skills<BR>are in the right places",
+		"970x250": "Work works when the right<BR>skills are in the right places"
+	},
+	"tagline": "HR, Pay, &<BR>Workforce<BR>Management",
+	"ctaText": "Learn more ",
+	"lineText": "Fatigue Analysis",
+	"lineTaskWord": "tasks",
+	"lineTaskTotal": {
+		"160x600": "out of 4 completed",
+		"300x250": "out of 4<BR>completed",
+		"300x600": "out of 4<BR>completed",
+		"728x90": "out of 4<BR>completed",
+		"970x250": "out of 4<BR>completed"
+	},
+	"barsText": "Overtime<BR>Hours",
+	"circleText": "Shifts Worked",
+	"animation1": "circle",
+	"animation2": "line",
+	"animation3": "bars"
+}
--- preview/UKG/UKG_970x250_Pay/main.js
+++ preview/UKG/UKG_970x250_Pay/main.js
@@ -2,7 +2,35 @@ window.onload = function () {
     init();
 }
 
+function loadLocalDynamic() {
+
+    var bannerW = document.querySelector("#banner").offsetWidth;
+    var bannerH = document.querySelector("#banner").offsetHeight;
+    var size = bannerW+"x"+bannerH;
+
+    document.querySelector('#bg').src = localDynamicData.bg;
+    document.querySelector('#logo').src = localDynamicData.logo;
+
+    document.querySelector('#text1').innerHTML = localDynamicData.text1[size];
+    document.querySelector('#tagline').innerHTML = localDynamicData.tagline;
+    document.querySelector('#ctaText').innerHTML = localDynamicData.ctaText;
+    
+    document.querySelector('#payrollText').innerHTML = localDynamicData.lineText;
+    document.querySelector('#payrollTaskWord').innerHTML = localDynamicData.lineTaskWord;
+    document.querySelector('#payrollTaskTotal').innerHTML = localDynamicData.lineTaskTotal[size];
+    document.querySelector('#headcountText').innerHTML = localDynamicData.barsText;
+    document.querySelector('#retentionText').innerHTML = localDynamicData.circleText;
+
+    var ani1 = localDynamicData.animation1;
+    var ani2 = localDynamicData.animation2;
+    var ani3 = localDynamicData.animation3;
+
+    getAnimation(ani1, ani2, ani3);
+}
+
 function init() {
+
+    loadLocalDynamic();
 
     var arrowTL = gsap.timeline({});
     arrowTL.to("#arrow", 0.2, {x:10, ease:Power2.easeInOut});
@@ -26,31 +54,37 @@ function init() {
         window.open(clickTag, '_blank');
         masterTL.progress(1).pause();
     })
-
-    getAnimation();
 }
 
 
-function getAnimation(){
+function getAnimation(ani1, ani2, ani3){
 
     var introTL = animateIntro();
-    var payrollTL = animatePayroll();
-    var headcountTL = animateHeadcount();
-    var retentionTL = animateRetention();
+    var payrollTL = animatePayroll(ani3);
+    var headcountTL = animateHeadcount(ani3);
+    var retentionTL = animateRetention(ani3);
 
-    masterTL = gsap.timeline({repeat:0});
+    masterTL = gsap.timeline({repeat:0, paused:true});
 
     masterTL.to("#loaderWrapper", 0.2, {opacity:0, ease:Sine.easeInOut, onComplete:function(){
         gsap.set("#loaderWrapper", {display:"none"});
     }}, 0)
     masterTL.add("start");
     masterTL.add(introTL, "start+=1");
-    masterTL.add(payrollTL);
-    masterTL.add(headcountTL);
-    masterTL.add(retentionTL);
-    
-    console.log(masterTL.duration());
-    
+
+    for (var i = 1; i < 4; i++) {        
+        if(eval("ani"+i) === "circle"){
+            masterTL.add(retentionTL);
+        }
+        else if(eval("ani"+i) === "line"){
+            masterTL.add(payrollTL);
+        }
+        else{
+            masterTL.add(headcountTL);
+        }
+    }
+
+    masterTL.play(0).duration(15);
 
 }
 
@@ -61,21 +95,21 @@ function animateIntro() {
     var text1Split = new SplitText("#text1", {type: "lines"});
 
     tl.add('start');
-    tl.to("#introPanel", 1, {height:42, ease:Power2.easeInOut}, "start");
-    tl.to("#logo", 1, {scale:0.45, ease:Power2.easeInOut}, "start");
+    tl.to("#introPanel", 1, {height:32, ease:Power2.easeInOut}, "start");
+    tl.to("#logo", 1, {scale:0.4, ease:Power2.easeInOut}, "start");
     tl.from(taglineSplit.lines, 0.5, {x:-100, stagger:0.2, ease:Sine.easeOut}, "start+=1");
     tl.from(text1Split.lines, 0.5, {y:10, opacity:0, stagger:0.2, ease:Sine.easeOut}, "start+=1");
     tl.from("#cta", 0.5, {y:100, ease:Power2.easeOut}, "start+=1");
-    tl.to("#bg", 0.75, {x:-300, ease:Power3.easeIn}, "start+=3");
+    tl.to("#bg", 0.75, {y:-250, ease:Power3.easeIn}, "start+=3");
 
     return tl;
 }
 
-function animatePayroll() {
+function animatePayroll(ani3) {
     var tl = gsap.timeline({repeat:0});
 
     tl.add('start');
-    tl.from("#payrollWrapper", 1, {x:300, ease:Power3.easeOut}, "start");
+    tl.from("#payrollWrapper", 1, {y:250, ease:Power3.easeOut}, "start");
     tl.from("#payrollLine", 1, {width:0, ease:Power2.easeInOut}, "start+=0.8");
     tl.from("#payrollTaskNr", 1,{textContent: 0, ease:Power2.easeInOut, snap: { textContent: 1 }, stagger: {
         each: 1.0,
@@ -84,12 +118,14 @@ function animatePayroll() {
         }
     }}, "start+=0.8");
     tl.from("#payrollBar", 0.7, {width:0}, "start+=1.1");
-    tl.to("#payrollWrapper", 1, {x:-300, ease:Power3.easeIn}, "start+=3");
+    if(ani3 != "line"){
+        tl.to("#payrollWrapper", 1, {y:-250, ease:Power3.easeIn}, "start+=3");
+    }
 
     return tl;
 }
 
-function animateHeadcount() {
+function animateHeadcount(ani3) {
     
     var tl = gsap.timeline({repeat:0});
 
@@ -97,20 +133,22 @@ function animateHeadcount() {
     var allLines = document.querySelectorAll(".line");
 
     tl.add('start');
-    tl.from("#headcountWrapper", 1, {x:300, ease:Power3.easeOut}, "start");
+    tl.from("#headcountWrapper", 1, {y:250, ease:Power3.easeOut}, "start");
     tl.from(allBars, 1, {height:0, stagger:0.1, ease:Power3.easeOut}, "start+=0.5");
     tl.from(allLines, 1, {height:0, stagger:0.08, ease:Power3.easeOut}, "start+=1");
-    tl.to("#headcountWrapper", 1, {x:-300, ease:Power3.easeIn}, "start+=3");
+    if(ani3 != "bars"){
+        tl.to("#headcountWrapper", 1, {y:-250, ease:Power3.easeIn}, "start+=3");
+    }
 
     return tl;
 }
 
-function animateRetention() {
+function animateRetention(ani3) {
 
     var tl = gsap.timeline({repeat:0});    
 
     tl.add('start');
-    tl.from("#retentionWrapper", 1, {x:300, ease:Power3.easeOut}, "start");
+    tl.from("#retentionWrapper", 1, {y:250, ease:Power3.easeOut}, "start");
     tl.from("#retentionSVG", 0.4, {scale:0, ease:Power2.easeOut}, "start+=0.6");
     tl.from("#retentionNr", 0.4, {scale:0, ease:Power2.easeOut}, "start+=0.7");
     tl.fromTo("#retentionLine", 1,{drawSVG: 0}, {drawSVG: "91%", ease:Power2.easeInOut}, "start+=1");
@@ -120,6 +158,11 @@ function animateRetention() {
             this.targets()[0].innerHTML = numberWithCommas(Math.ceil(this.targets()[0].textContent));
         }
     }}, "start+=1");
+    
+    if(ani3 != "circle"){
+        tl.to("#retentionWrapper", 1, {y:-250, ease:Power3.easeIn}, "start+=3");
+    }
+    
 
     return tl;
 }
