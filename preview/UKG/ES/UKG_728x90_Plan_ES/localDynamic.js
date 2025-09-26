7beb7914297de7a402ef28205e945ad38d9626e5
Dave Tervoort <dave.tervoort@accenture.com>
2025-09-26 17:21:09 +0200

    UKG ES done

---
+++ preview/UKG/ES/UKG_728x90_Plan_ES/localDynamic.js
@@ -0,0 +1,36 @@
+localDynamicData = {
+	"variationName": "ES_Plan",
+	"campaignName": "ES",
+	"dimensionName": {
+		"160x600": "UKG_160x600_Plan_ES",
+		"300x250": "UKG_300x250_Plan_ES",
+		"300x600": "UKG_300x600_Plan_ES",
+		"728x90": "UKG_728x90_Plan_ES",
+		"970x250": "UKG_970x250_Plan_ES"
+	},
+	"bg": "./bg.jpg",
+	"logo": "./logo.png",
+	"text1": {
+		"160x600": "El trabajo funciona cuando tienes una visión clara del equipo humano",
+		"300x250": "El trabajo funciona cuando tienes una visión clara del equipo humano",
+		"300x600": "El trabajo funciona cuando tienes una visión clara del equipo humano",
+		"728x90": "El trabajo funciona cuando tienes una visión clara del equipo humano",
+		"970x250": "El trabajo funciona cuando tienes una visión clara del equipo humano"
+	},
+	"tagline": "RR. HH., nóminas<BR>y gestión de<BR>personal",
+	"ctaText": "Descubrir más ",
+	"lineText": "Productivity Rate",
+	"lineTaskWord": "tareas",
+	"lineTaskTotal": {
+		"160x600": "de 4 completadas",
+		"300x250": "de 4<BR>completadas",
+		"300x600": "de 4<BR>completadas",
+		"728x90": "de 4<BR>completadas",
+		"970x250": "de 4<BR>completadas"
+	},
+	"barsText": "Global<BR>Headcount",
+	"circleText": "Retención de personal",
+	"animation1": "bars",
+	"animation2": "circle",
+	"animation3": "line"
+}
---
+++ preview/UKG/ES/UKG_970x250_Schedule_ES/localDynamic.js
@@ -0,0 +1,36 @@
+localDynamicData = {
+	"variationName": "ES_Schedule",
+	"campaignName": "ES",
+	"dimensionName": {
+		"160x600": "UKG_160x600_Schedule_ES",
+		"300x250": "UKG_300x250_Schedule_ES",
+		"300x600": "UKG_300x600_Schedule_ES",
+		"728x90": "UKG_728x90_Schedule_ES",
+		"970x250": "UKG_970x250_Schedule_ES"
+	},
+	"bg": "./bg.jpg",
+	"logo": "./logo.png",
+	"text1": {
+		"160x600": "El trabajo funciona cuando el talento está donde más se necesita",
+		"300x250": "El trabajo funciona cuando el talento está donde más se necesita",
+		"300x600": "El trabajo funciona cuando el talento está donde más se necesita",
+		"728x90": "El trabajo funciona cuando el talento está donde más se necesita",
+		"970x250": "El trabajo funciona cuando el talento está donde más se necesita"
+	},
+	"tagline": "RR. HH., nóminas<BR>y gestión de<BR>personal",
+	"ctaText": "Descubrir más ",
+	"lineText": "Análisis de la fatiga laboral",
+	"lineTaskWord": "tareas",
+	"lineTaskTotal": {
+		"160x600": "de 4 completadas",
+		"300x250": "de 4<BR>completadas",
+		"300x600": "de 4<BR>completadas",
+		"728x90": "de 4<BR>completadas",
+		"970x250": "de 4<BR>completadas"
+	},
+	"barsText": "Horas extra",
+	"circleText": "Turnos trabajados",
+	"animation1": "circle",
+	"animation2": "line",
+	"animation3": "bars"
+}
