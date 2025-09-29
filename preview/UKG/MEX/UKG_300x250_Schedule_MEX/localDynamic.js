eb6b60324b059468f36a7ed6430868a113677657
Dave Tervoort <dave.tervoort@accenture.com>
2025-09-29 17:01:21 +0200

    UKG MEX done

---
+++ preview/UKG/MEX/UKG_300x250_Schedule_MEX/localDynamic.js
@@ -0,0 +1,36 @@
+localDynamicData = {
+	"variationName": "MEX_Schedule",
+	"campaignName": "MEX",
+	"dimensionName": {
+		"160x600": "UKG_160x600_Schedule_MEX",
+		"300x250": "UKG_300x250_Schedule_MEX",
+		"300x600": "UKG_300x600_Schedule_MEX",
+		"728x90": "UKG_728x90_Schedule_MEX",
+		"970x250": "UKG_970x250_Schedule_MEX"
+	},
+	"bg": "./bg.jpg",
+	"logo": "./logo.png",
+	"text1": {
+		"160x600": "El trabajo funciona cuando el talento correcto está en el lugar indicado",
+		"300x250": "El trabajo funciona cuando el talento correcto está en el lugar indicado",
+		"300x600": "El trabajo funciona cuando el talento correcto está en el lugar indicado",
+		"728x90": "El trabajo funciona cuando el talento correcto está en el lugar indicado",
+		"970x250": "El trabajo funciona cuando el talento correcto está en el lugar indicado"
+	},
+	"tagline": "Gestión de RR.HH.,<BR>Nómina y<BR>Fuerza Laboral",
+	"ctaText": "Más información ",
+	"lineText": "Análisis de fatiga",
+	"lineTaskWord": "tareas",
+	"lineTaskTotal": {
+		"160x600": "de 4 completadas",
+		"300x250": "de 4<BR>completadas",
+		"300x600": "de 4<BR>completadas",
+		"728x90": "de 4<BR>completadas",
+		"970x250": "de 4<BR>completadas"
+	},
+	"barsText": "Horas extra",
+	"circleText": "Turnos trabajados",
+	"animation1": "circle",
+	"animation2": "line",
+	"animation3": "bars"
+}
---
+++ preview/UKG/MEX/UKG_320x50_MEX/localDynamic.js
@@ -0,0 +1,39 @@
+localDynamicData = {
+	"variationName": "MEX_Pay",
+	"campaignName": "MEX",
+	"dimensionName": {
+		"160x600": "UKG_160x600_Pay_MEX",
+		"300x250": "UKG_300x250_Pay_MEX",
+		"300x600": "UKG_300x600_Pay_MEX",
+		"320x50": "UKG_320x50_MEX",
+		"728x90": "UKG_728x90_Pay_MEX",
+		"970x250": "UKG_970x250_Pay_MEX"
+	},
+	"bg": "./bg.jpg",
+	"logo": "./logo.png",
+	"text1": {
+		"160x600": "El trabajo funciona cuando la nómina se gestiona de forma inteligente",
+		"300x250": "El trabajo funciona cuando la nómina se gestiona de forma inteligente",
+		"300x600": "El trabajo funciona cuando la nómina se gestiona de forma inteligente",
+		"320x50": "El trabajo funciona cuando la nómina se gestiona de forma inteligente",
+		"728x90": "El trabajo funciona cuando la nómina se gestiona de forma inteligente",
+		"970x250": "El trabajo funciona cuando la nómina se gestiona de forma inteligente"
+	},
+	"tagline": "Gestión de RR.HH.,<BR>Nómina y<BR>Fuerza Laboral",
+	"ctaText": "Más información ",
+	"lineText": "Estado de la nómina",
+	"lineTaskWord": "tareas",
+	"lineTaskTotal": {
+		"160x600": "de 4 completadas",
+		"300x250": "de 4<BR>completadas",
+		"300x600": "de 4<BR>completadas",
+		"320x50": "de 4<BR>completadas",
+		"728x90": "de 4<BR>completadas",
+		"970x250": "de 4<BR>completadas"
+	},
+	"barsText": "Número de empleados",
+	"circleText": "Retención de personal",
+	"animation1": "line",
+	"animation2": "bars",
+	"animation3": "circle"
+}
