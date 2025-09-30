d0aed87499be70b5f6b48fc189b1b56c9c76f08c
Dave Tervoort <dave.tervoort@accenture.com>
2025-09-30 09:13:54 +0200

    UKG cleanup and seperate UK folder

---
+++ preview/UKG/UK/UKG_300x600_Pay_UK/localDynamic.js
@@ -0,0 +1,39 @@
+localDynamicData = {
+	"variationName": "UK_Pay",
+	"campaignName": "UK",
+	"dimensionName": {
+		"160x600": "UKG_160x600_Pay_UK",
+		"300x250": "UKG_300x250_Pay_UK",
+		"300x600": "UKG_300x600_Pay_UK",
+		"320x50": "UKG_320x50_UK",
+		"728x90": "UKG_728x90_Pay_UK",
+		"970x250": "UKG_970x250_Pay_UK"
+	},
+	"bg": "./bg.jpg",
+	"logo": "./logo.png",
+	"text1": {
+		"160x600": "Work works<BR>better when<BR>payroll runs<BR>smarter",
+		"300x250": "Work works<BR>better when<BR>payroll runs<BR>smarter",
+		"300x600": "Work works better<BR>when payroll runs<BR>smarter",
+		"320x50": "Work works better when<BR>payroll runs smarter",
+		"728x90": "Work works better when<BR>payroll runs smarter",
+		"970x250": "Work works better when<BR>payroll runs smarter"
+	},
+	"tagline": "HR, Pay, &<BR>Workforce<BR>Management",
+	"ctaText": "Learn more ",
+	"lineText": "Payroll Status",
+	"lineTaskWord": "tasks",
+	"lineTaskTotal": {
+		"160x600": "out of 4 completed",
+		"300x250": "out of 4<BR>completed",
+		"300x600": "out of 4<BR>completed",
+		"320x50": "out of 4<BR>completed",
+		"728x90": "out of 4<BR>completed",
+		"970x250": "out of 4<BR>completed"
+	},
+	"barsText": "Headcount",
+	"circleText": "Retention",
+	"animation1": "line",
+	"animation2": "bars",
+	"animation3": "circle"
+}
---
+++ preview/UKG/UK/UKG_728x90_Plan_UK/localDynamic.js
@@ -0,0 +1,36 @@
+localDynamicData = {
+	"variationName": "UK_Plan",
+	"campaignName": "UK",
+	"dimensionName": {
+		"160x600": "UKG_160x600_Plan_UK",
+		"300x250": "UKG_300x250_Plan_UK",
+		"300x600": "UKG_300x600_Plan_UK",
+		"728x90": "UKG_728x90_Plan_UK",
+		"970x250": "UKG_970x250_Plan_UK"
+	},
+	"bg": "./bg.jpg",
+	"logo": "./logo.png",
+	"text1": {
+		"160x600": "Work works<BR>when workforce<BR>understanding<BR>is at your<BR>fingertips",
+		"300x250": "Work works<BR>when workforce<BR>understanding is<BR>at your fingertips",
+		"300x600": "Work works<BR>when workforce<BR>understanding is<BR>at your fingertips",
+		"728x90": "Work works when workforce<BR>understanding is at your fingertips",
+		"970x250": "Work works when<BR>workforce understanding<BR>is at your fingertips"
+	},
+	"tagline": "HR, Pay, &<BR>Workforce<BR>Management",
+	"ctaText": "Learn more ",
+	"lineText": "Productivity Rate",
+	"lineTaskWord": "tasks",
+	"lineTaskTotal": {
+		"160x600": "out of 4 completed",
+		"300x250": "out of 4<BR>completed",
+		"300x600": "out of 4<BR>completed",
+		"728x90": "out of 4<BR>completed",
+		"970x250": "out of 4<BR>completed"
+	},
+	"barsText": "Global<BR>Headcount",
+	"circleText": "Retention",
+	"animation1": "bars",
+	"animation2": "circle",
+	"animation3": "line"
+}
