52f251f5c10037be8b168165ce396e9c9911f15d
Dave Tervoort <dave.tervoort@accenture.com>
2025-09-30 14:36:01 +0200

    UKG DE done

---
+++ preview/UKG/DE/UKG_300x250_Plan_DE/localDynamic.js
@@ -0,0 +1,36 @@
+localDynamicData = {
+	"variationName": "DE_Plan",
+	"campaignName": "DE",
+	"dimensionName": {
+		"160x600": "UKG_160x600_Plan_DE",
+		"300x250": "UKG_300x250_Plan_DE",
+		"300x600": "UKG_300x600_Plan_DE",
+		"728x90": "UKG_728x90_Plan_DE",
+		"970x250": "UKG_970x250_Plan_DE"
+	},
+	"bg": "./bg.jpg",
+	"logo": "./logo.png",
+	"text1": {
+		"160x600": "Arbeit funktioniert, wenn die Mitarbeitenden verstanden werden",
+		"300x250": "Arbeit funktioniert, wenn die Mitarbeitenden verstanden werden",
+		"300x600": "Arbeit funktioniert, wenn die Mitarbeitenden verstanden werden",
+		"728x90": "Arbeit funktioniert, wenn die Mitarbeitenden verstanden werden",
+		"970x250": "Arbeit funktioniert, wenn die Mitarbeitenden verstanden werden"
+	},
+	"tagline": "HR, Payroll &<BR>Workforce<BR>Management",
+	"ctaText": "Mehr erfahren ",
+	"lineText": "Produktivitätsrate",
+	"lineTaskWord": "Aufgaben",
+	"lineTaskTotal": {
+		"160x600": "von 4 erledigt",
+		"300x250": "von 4<BR>erledigt",
+		"300x600": "von 4<BR>erledigt",
+		"728x90": "von 4<BR>erledigt",
+		"970x250": "von 4<BR>erledigt"
+	},
+	"barsText": "Anzahl der Mitarbeiter weltweit",
+	"circleText": "Personalbindung",
+	"animation1": "bars",
+	"animation2": "circle",
+	"animation3": "line"
+}
---
+++ preview/UKG/DE/UKG_320x50_DE/localDynamic.js
@@ -0,0 +1,39 @@
+localDynamicData = {
+	"variationName": "DE_Pay",
+	"campaignName": "DE",
+	"dimensionName": {
+		"160x600": "UKG_160x600_Pay_DE",
+		"300x250": "UKG_300x250_Pay_DE",
+		"300x600": "UKG_300x600_Pay_DE",
+		"320x50": "UKG_320x50_DE",
+		"728x90": "UKG_728x90_Pay_DE",
+		"970x250": "UKG_970x250_Pay_DE"
+	},
+	"bg": "./bg.jpg",
+	"logo": "./logo.png",
+	"text1": {
+		"160x600": "Arbeit funktioniert besser, wenn die Gehalt-sabrechnung intelligenter abgewickelt wird",
+		"300x250": "Arbeit funktioniert besser, wenn die Gehalt-sabrechnung intelligenter abgewickelt wird",
+		"300x600": "Arbeit funktioniert besser, wenn die Gehaltsabrechnung intelligenter abgewickelt wird",
+		"320x50": "Arbeit funktioniert besser, wenn die Gehaltsabrechnung intelligenter abgewickelt wird",
+		"728x90": "Arbeit funktioniert besser, wenn die Gehaltsabrechnung intelligenter abgewickelt wird",
+		"970x250": "Arbeit funktioniert besser, wenn die Gehaltsabrechnung intelligenter abgewickelt wird"
+	},
+	"tagline": "HR, Payroll &<BR>Workforce<BR>Management",
+	"ctaText": "Mehr erfahren ",
+	"lineText": "Statut de la paie",
+	"lineTaskWord": "Aufgaben",
+	"lineTaskTotal": {
+		"160x600": "von 4 erledigt",
+		"300x250": "von 4<BR>erledigt",
+		"300x600": "von 4<BR>erledigt",
+		"320x50": "von 4<BR>erledigt",
+		"728x90": "von 4<BR>erledigt",
+		"970x250": "von 4<BR>erledigt"
+	},
+	"barsText": "Effectif",
+	"circleText": "Personalbindung",
+	"animation1": "line",
+	"animation2": "bars",
+	"animation3": "circle"
+}
