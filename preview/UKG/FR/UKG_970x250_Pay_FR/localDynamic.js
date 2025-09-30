e77f45c467ae15c75cb29fc9cfa667ff7c33b09a
Dave Tervoort <dave.tervoort@accenture.com>
2025-09-30 11:04:52 +0200

    UKG FR done

---
+++ preview/UKG/FR/UKG_970x250_Pay_FR/localDynamic.js
@@ -0,0 +1,39 @@
+localDynamicData = {
+	"variationName": "FR_Pay",
+	"campaignName": "FR",
+	"dimensionName": {
+		"160x600": "UKG_160x600_Pay_FR",
+		"300x250": "UKG_300x250_Pay_FR",
+		"300x600": "UKG_300x600_Pay_FR",
+		"320x50": "UKG_320x50_FR",
+		"728x90": "UKG_728x90_Pay_FR",
+		"970x250": "UKG_970x250_Pay_FR"
+	},
+	"bg": "./bg.jpg",
+	"logo": "./logo.png",
+	"text1": {
+		"160x600": "Une paie plus intelligente, c'est un travail plus fluide",
+		"300x250": "Une paie plus intelligente, c'est un travail plus fluide",
+		"300x600": "Une paie plus intelligente, c'est<BR>un travail plus<BR>fluide",
+		"320x50": "Une paie plus intelligente, c'est un travail plus fluide",
+		"728x90": "Une paie plus intelligente, c'est un<BR>travail plus fluide",
+		"970x250": "Une paie plus intelligente, c'est un travail plus fluide"
+	},
+	"tagline": "RH, Paie, Gestion<BR>et Planification<BR>des effectifs",
+	"ctaText": "En savoir plus ",
+	"lineText": "Statut de la paie",
+	"lineTaskWord": "tâches",
+	"lineTaskTotal": {
+		"160x600": "sur 4 réalisées",
+		"300x250": "sur 4<BR>réalisées",
+		"300x600": "sur 4<BR>réalisées",
+		"320x50": "sur 4<BR>réalisées",
+		"728x90": "sur 4<BR>réalisées",
+		"970x250": "sur 4<BR>réalisées"
+	},
+	"barsText": "Effectif",
+	"circleText": "Taux de rétention",
+	"animation1": "line",
+	"animation2": "bars",
+	"animation3": "circle"
+}
