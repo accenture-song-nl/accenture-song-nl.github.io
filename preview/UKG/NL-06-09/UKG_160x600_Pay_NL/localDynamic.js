0632600fe6fc25db7bb125ce5a2afef602dae480
Dave Tervoort <dave.tervoort@accenture.com>
2025-10-06 13:52:39 +0200

    NL extra preview

---
+++ preview/UKG/NL-06-09/UKG_160x600_Pay_NL/localDynamic.js
@@ -0,0 +1,39 @@
+localDynamicData = {
+	"variationName": "NL_Pay",
+	"campaignName": "NL",
+	"dimensionName": {
+		"160x600": "UKG_160x600_Pay_NL",
+		"300x250": "UKG_300x250_Pay_NL",
+		"300x600": "UKG_300x600_Pay_NL",
+		"320x50": "UKG_320x50_NL",
+		"728x90": "UKG_728x90_Pay_NL",
+		"970x250": "UKG_970x250_Pay_NL"
+	},
+	"bg": "./bg.jpg",
+	"logo": "./logo.png",
+	"text1": {
+		"160x600": "Werk werkt<BR>beter als de<BR>salaris-administratie slim geregeld is",
+		"300x250": "Werk werkt<BR>beter als de<BR>salaris-<BR>administratie<BR>slim geregeld is",
+		"300x600": "Werk werkt<BR>beter als de<BR>salarisadministratie slim geregeld is",
+		"320x50": "Werk werkt beter als de salarisadministratie slim geregeld is",
+		"728x90": "Werk werkt beter als de salaris-<BR>administratie slim geregeld is",
+		"970x250": "Werk werkt beter als de salarisadministratie slim geregeld is"
+	},
+	"tagline": "HR, Payroll- en<BR>Workforce<BR>Management",
+	"ctaText": "Meer informatie ",
+	"lineText": "Salarisstatus",
+	"lineTaskWord": "van de",
+	"lineTaskTotal": {
+		"160x600": "4 taken voltooid",
+		"300x250": "4 taken<BR>voltooid",
+		"300x600": "4 taken<BR>voltooid",
+		"320x50": "4 taken<BR>voltooid",
+		"728x90": "4 taken<BR>voltooid",
+		"970x250": "4 taken<BR>voltooid"
+	},
+	"barsText": "Aantal medewerkers",
+	"circleText": "Behoud",
+	"animation1": "line",
+	"animation2": "bars",
+	"animation3": "circle"
+}
